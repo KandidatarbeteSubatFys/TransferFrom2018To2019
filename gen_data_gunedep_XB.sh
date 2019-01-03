3307710952dfdca4edf484ccd189b1d25257984c
JakobWadman <35529077+JakobWadman@users.noreply.github.com>
2019-01-04 00:47:35 +0100

    Create gen_data_gunedep_XB.sh

---
+++ gen_data_gunedep_XB.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# $1 is root file from simulation with XB
+
+# This code is for XB only
+
+root -q 'root_make_class_and_gen_data_files_XB.C('\"$1\"')'
+echo "ROOT finished"
+echo "Removing h102.C and h102.h"
+rm h102.C
+rm h102.h
