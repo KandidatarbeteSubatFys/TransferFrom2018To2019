a348f2a9ea96ead36e88726128893c3df61142a8
JakobWadman <35529077+JakobWadman@users.noreply.github.com>
2019-01-18 05:29:29 +0100

    Update job_script.sh

--- Kebnekaise/job_script.sh
+++ Kebnekaise/job_script.sh
@@ -3,5 +3,5 @@
 module load GCC/6.4.0-2.28  CUDA/9.0.176  OpenMPI/2.1.1
 module load TensorFlow/1.5.0-Python-3.6.3
 
-# Here we specify which script we want to run
+# Here you specify which script you want to run
 python3 job.py
